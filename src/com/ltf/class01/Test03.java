579797e9425d47344e09a434de73df29ebbedb66
849673404 <849673404@qq.com>
2017-11-09 13:39:42 +0800

    删除了数据格式的代码

--- src/com/ltf/class01/Test03.java
+++ src/com/ltf/class01/Test03.java
@@ -3,14 +3,5 @@ package com.ltf.class01;
 /**
 	基本数据类型转换
 */
-public class Test03{
-	public static void main(String[] args){
-		for(int i=1;i<100000;i++){
-			int m = (int) Math.sqrt(100+i);
-			int n = (int) Math.sqrt(168+i);
-			if(m*m==(i+100)&&(n*n)==(i+168)){
-				System.out.println("输出的数："+i);
-			}
-		}
-	}
+
 }
